b92ae26bb76e660157ab1deed92fd812247ba13c
Herb Miller <herb@bobbingwide.com>
2025-01-05 16:45:34 +0000

    Set both start and end dates and the post_date #1

--- admin/class-ai1ec-migration.php
+++ admin/class-ai1ec-migration.php
@@ -3,6 +3,7 @@
 class ai1ec_migration {
 
 	private $avents;
+	private $post_date = null;
 
 	function __construct() {
 
@@ -18,16 +19,18 @@ class ai1ec_migration {
 	function process_avents() {
 		$loop = 0;
 		foreach ( $this->avents as $avent ) {
-			e( "Processing: " . $avent->ID );
+			e( "Processing:  {$avent->ID}  {$avent->post_title}" );
 			br();
 			$fields = $this->get_event_fields( $avent->ID );
 			if ( $fields ) {
 				$this->map_event_fields( $fields );
+				$this->set_post_date( $fields->start );
 			}
+
 			$this->update_post( $avent );
 			//bw_flush();
 			$loop++;
-			if ( $loop >= 10 ) {
+			if ( $loop >= 30 ) {
 				break;
 			}
 		}
@@ -51,7 +54,7 @@ class ai1ec_migration {
 	function get_event_fields( $id ) {
 		global $wpdb;
 		$query = $wpdb->prepare( "SELECT * from {$wpdb->prefix}ai1ec_events where post_id = %d", $id);
-		e( $query);
+		//e( $query);
 		$vars = $wpdb->get_results( $query);
 		if ( $vars && count( $vars )) {
 			$fields=$vars[0];
@@ -62,8 +65,7 @@ class ai1ec_migration {
 	}
 
 	function map_event_fields( $fields ) {
-		print_r( $fields);
-		$_POST['_address'] = $fields->venue . ' ' . $fields->address;
+		$print__POST['_address'] = $fields->venue . ' ' . $fields->address;
 		$_POST['_post_code'] = $fields->postal_code;
 		$_POST['_contact_name'] = $fields->contact_name;
 		$_POST['_contact_phone'] = $fields->contact_phone;
@@ -71,12 +73,14 @@ class ai1ec_migration {
 		$_POST['_cost'] = $this->get_cost( $fields->cost);
 		$_POST['_contact_url'] = $fields->contact_url;
 		$_POST['ticket_url'] = $fields->ticket_url;
-		$_POST['_date'] = $this->get_date( $fields->start );
+		$_POST['_start_date'] = $this->get_date( $fields->start );
+		$_POST['_end_date'] = $this->get_date( $fields->end );
 		$_POST['_lat'] = $this->get_latorlong( $fields->latitude );
 		$_POST['_long'] = $this->get_latorlong( $fields->longitude );
 		if ( $fields->allday) {
 			$_POST['_start_time'] = '00:00';
 			$_POST['_end_time'] = '23:59';
+			//$_POST['_date'] = $this->get_date( $fields->end );
  		}
 		else {
 			$_POST['_start_time']=$this->get_time( $fields->start );
@@ -90,7 +94,7 @@ class ai1ec_migration {
 
 	function get_cost( $cost ) {
 		$cost_fields = unserialize( $cost);
-		print_r( $cost_fields);
+		//print_r( $cost_fields);
 		if ( $cost_fields['is_free']) {
 			if ( empty( $cost_fields['cost'] ) ) {
 				$cost_fields['cost']='Free';
@@ -113,6 +117,10 @@ class ai1ec_migration {
 		return $time;
 	}
 
+	function set_post_date( $start ) {
+		$this->post_date = bw_format_date( $start, "Y-m-d H:i:s");
+	}
+
 	function get_latorlong( $latorlong ) {
 		$value = ( $latorlong == 0.0 ) ? '' : $latorlong;
 		return $value;
@@ -121,6 +129,7 @@ class ai1ec_migration {
 	function update_post( $avent, $post_type='event' ) {
 		$post = $avent;
 		$post->post_type = $post_type;
+		$post->post_date = $this->post_date;
 		//print_r( $post );
 		wp_update_post( $avent );
 	}
